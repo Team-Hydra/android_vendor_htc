cd42316a5a4864c3421faf7aa5d214d6de21467a
Matt Filetto <matt.filetto@gmail.com>
2014-01-12 00:10:27 -0800

    pyramid: use prebuilt Camera2.apk that is known to be bug free for now
    Change-Id: Ic4dbf0104de8ae55880fb053dd52df8b6ce1080e

--- pyramid/pyramid-vendor-blobs.mk
+++ pyramid/pyramid-vendor-blobs.mk
@@ -74,4 +74,6 @@ PRODUCT_COPY_FILES += \
   vendor/htc/pyramid/proprietary/lib/libmllite.so:system/lib/libmllite.so \
   vendor/htc/pyramid/proprietary/lib/libmlplatform.so:system/lib/libmlplatform.so \
   vendor/htc/pyramid/proprietary/lib/libmpl_jni.so:system/lib/libmpl_jni.so \
-  vendor/htc/pyramid/proprietary/lib/libmpl.so:system/lib/libmpl.so
+  vendor/htc/pyramid/proprietary/lib/libmpl.so:system/lib/libmpl.so \
+  vendor/htc/pyramid/proprietary/app/Camera2.apk:system/app/Camera2.apk \
+  vendor/htc/pyramid/proprietary/app/Camera2.apk:system/priv-app/Camera2.apk
